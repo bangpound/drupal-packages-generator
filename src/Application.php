b91c3e8b7d226eea10a138b1f81c2c553745f625
Benjamin Doherty <bjd@bangpound.org>
2014-09-20 11:37:30 -0500

    single command application.
    based on http://symfony.com/doc/current/components/console/single_command_tool.html

--- src/Application.php
+++ src/Application.php
@@ -4,14 +4,49 @@
 namespace kasperg\DrupalPackagesGenerator;
 
 use \Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputInterface;
 
 class Application extends BaseApplication {
 
-    public function __construct()
+    /**
+     * Gets the name of the command based on input.
+     *
+     * @param InputInterface $input The input interface
+     *
+     * @return string The command name
+     */
+    protected function getCommandName(InputInterface $input)
     {
-        parent::__construct();
-
-        $this->add(new GeneratePackagesCommand());
+        // This should return the name of your command.
+        return 'drupal-packages-generator:generate-packages';
     }
 
+    /**
+     * Gets the default commands that should always be available.
+     *
+     * @return array An array of default Command instances
+     */
+    protected function getDefaultCommands()
+    {
+        // Keep the core default commands to have the HelpCommand
+        // which is used when using the --help option
+        $defaultCommands = parent::getDefaultCommands();
+
+        $defaultCommands[] = new GeneratePackagesCommand();
+
+        return $defaultCommands;
+    }
+
+    /**
+     * Overridden so that the application doesn't expect the command
+     * name to be the first argument.
+     */
+    public function getDefinition()
+    {
+        $inputDefinition = parent::getDefinition();
+        // clear out the normal first argument, which is the command name
+        $inputDefinition->setArguments();
+
+        return $inputDefinition;
+    }
 } 
